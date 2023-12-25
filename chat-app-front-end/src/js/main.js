6502b76d5d074a183db53d6fe1efada4959e8c68
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2023-12-26 02:07:28 +0530

    manage chat in two sides

--- chat-app-front-end/src/js/main.js
+++ chat-app-front-end/src/js/main.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signOut } from 'firebase/auth';
+import { GoogleAuthProvider,onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../firebase.js';
 
 const accountElm = document.querySelector("#account");
@@ -6,12 +6,15 @@ const userNameElm = document.querySelector("#user-name");
 const userEmailElm = document.querySelector("#user-email");
 const btnSignOutElm = document.querySelector("#btn-sign-out");
 const loaderElm = document.querySelector("#loader");
+const loginOverlayElm = document.querySelector("#login-overlay");
+const outputElm = document.querySelector("#output");
 const { API_BASE_URL } = process.env;
 const user = {
     email: null,
     name: null,
     picture: null
 };
+let ws = null;
 
 accountElm.addEventListener('click', (e)=> {
     accountElm.querySelector("#account-details")
@@ -30,4 +33,50 @@ btnSignOutElm.addEventListener('click', (e)=> {
     e.stopPropagation();
     signOut(auth);
 });
+onAuthStateChanged(auth, (loggedUser) => {
+    loaderElm.classList.add('d-none');
+    if (loggedUser){
+        user.email = loggedUser.email;
+        user.name = loggedUser.displayName;
+        user.picture = loggedUser.photoURL;
+        finalizeLogin();
+        loginOverlayElm.classList.add('d-none');
+        if (!ws){
+            ws = new WebSocket(`${API_BASE_URL}/messages`);
+            ws.addEventListener('message', loadNewChatMessages);
+            ws.addEventListener('error', ()=>{
+                alert("Connection failure, try refreshing the application");
+            });
+        }
+    } else{
+        user.email = null;
+        user.name = null;
+        user.picture = null;
+        loginOverlayElm.classList.remove('d-none');
+        if (ws){
+            ws.close();
+            ws = null;
+        }
+    }
+});
+function addChatMessageRecord({message, email}) {
+    const messageElm = document.createElement('div');
+    messageElm.classList.add('message')
+    if (email === user.email){
+        messageElm.classList.add('me');
+    }else{
+        messageElm.classList.add('others');
+    }
+    outputElm.append(messageElm);
+    messageElm.innerText = message;
+}
+function finalizeLogin(){
+    userNameElm.innerText = user.name;
+    userEmailElm.innerText = user.email;
+    accountElm.style.backgroundImage = `url(${user.picture})`;
+}
+function loadNewChatMessages(e){
+    const msg = JSON.parse(e.data);
+    addChatMessageRecord(msg);
+}
 
